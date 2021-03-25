db9213b9f6eaf5766a71ccb17c1cf796522c19e4
maxly20 <maxly2011@gmail.com>
2021-03-25 15:34:27 +1100

    adding responsive products carousel

--- app.js
+++ app.js
@@ -107,5 +107,30 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
