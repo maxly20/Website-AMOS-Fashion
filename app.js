4b6929eb3eed87b3a930294a08817a8b26743201
maxly20 <maxly2011@gmail.com>
2021-03-25 15:37:16 +1100

    fixing responsive carousel

--- app.js
+++ app.js
@@ -109,12 +109,24 @@ $(document).ready(function () {
     slidesToScroll: 1,
     responsive: [
       {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
         },
       },
       {
